423215298082f5ae9332716b4fd7b9d5ba665f38
nikita <neverdoit01@gmail.com>
2024-10-29 04:31:00 +0300

    add filter-settings

--- frontend/src/app/advertisement-list/advertisement-list.component.ts
+++ frontend/src/app/advertisement-list/advertisement-list.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {FilterSettingsComponent} from "../filter-settings/filter-settings.component";
+import {FilterService} from "../services/filter.service";
+
 
 @Component({
   selector: 'app-advertisement-list',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    FilterSettingsComponent
+  ],
   templateUrl: './advertisement-list.component.html',
   styleUrl: './advertisement-list.component.css'
 })
 export class AdvertisementListComponent {
+  searchQuery: string = '';
+
+  constructor(private filterService: FilterService) {}
+
+  openModal() {
+
+    this.filterService.open('Это важное уведомление!');
+  }
+
+  onSearch() {
+    console.log('Значение input:', this.searchQuery); // Здесь можно выполнить действие с значением
+    // Логика, связанная с отправкой значения
+  }
+
 
 }
